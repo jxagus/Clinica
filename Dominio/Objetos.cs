3093861bd688362b0bdb40ebcebe7bee854a9889
agust <agustin198ar@gmail.com>
2025-03-31 10:47:55 -0300

    update

---
+++ Dominio/Objetos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Objetos
+    {
+        public string Nombre { get; set; }
+        public int Id { get; set; }
+        [DisplayName("Número de cuenta")]
+        public int Saldo { get; set; }
+        //public int MyProperty { get; set; }
+
+    }
+}
